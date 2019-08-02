0759c051c267c26139eb25c4f316e6b17f12aa11
Junaid Babatunde <junaidtunde1@gmail.com>
2019-08-02 16:40:55 +0100

    add build folder

---
+++ client/build/precache-manifest.90514bc9d2fce008a408e0fc1c64e9d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253d86643c8f235cdfddf74b49398984",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d43672533bf8d43eb7ac",
+    "url": "/static/css/2.637a79cb.chunk.css"
+  },
+  {
+    "revision": "5e4972fbcc32a218ad77",
+    "url": "/static/css/main.a4432a58.chunk.css"
+  },
+  {
+    "revision": "d43672533bf8d43eb7ac",
+    "url": "/static/js/2.e7630a73.chunk.js"
+  },
+  {
+    "revision": "5e4972fbcc32a218ad77",
+    "url": "/static/js/main.17621939.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
